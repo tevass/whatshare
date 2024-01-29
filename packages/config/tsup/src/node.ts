7d9f886eded4e38a9c57f93ed5c2c227f02259f6
Estevão <estevao.biondi@gmail.com>
2024-01-29 19:43:19 -0300

    fix/chore: building alias paths

--- packages/config/tsup/src/node.ts
+++ packages/config/tsup/src/node.ts
@@ -1,7 +1,15 @@
+import { replaceTscAliasPaths } from 'tsc-alias'
 import type { Options } from 'tsup'
 
 export const config: Options = {
   format: ['cjs', 'esm'],
   dts: true,
   clean: true,
+  bundle: false,
+  outDir: 'dist',
+  async onSuccess() {
+    await replaceTscAliasPaths({
+      outDir: 'dist',
+    })
+  },
 }
